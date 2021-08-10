5646827650d64b032c7adc354e4dc3e5e54552df
SambacFeng <feng20000310@qq.com>
2021-08-10 23:54:46 +0800

    Create 105.js

---
+++ 105.js
@@ -0,0 +1,35 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {number[]} preorder
+ * @param {number[]} inorder
+ * @return {TreeNode}
+ */
+var buildTree = function (preorder, inorder) {
+    return build(preorder, 0, preorder.length - 1, inorder, 0, inorder.length - 1)
+};
+
+let build = (preorder, preLeft, preRight, inorder, inLeft, inRight) => {
+    if (preLeft <= preRight) {
+        let val = preorder[preLeft]
+        let index = 0
+        for (let i = inLeft; i <= inRight; i++) {
+            if (val === inorder[i]) {
+                index = i
+                break
+            }
+        }
+        let root = new TreeNode(val)
+        let leftSize = index - inLeft
+        root.left = build(preorder, preLeft + 1, preLeft + leftSize, inorder, inLeft, index - 1)
+        root.right = build(preorder, preorder + leftSize + 1, preRight, inorder, index + 1, inRight)
+        return root
+    }
+    return null
+}
