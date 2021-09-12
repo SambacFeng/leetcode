8b87d4710ad201080fd19083204343785684624e
SambacFeng <feng20000310@qq.com>
2021-09-12 22:50:49 +0800

    Create 200.js

---
+++ 200.js
@@ -0,0 +1,46 @@
+/**
+ * @param {character[][]} grid
+ * @return {number}
+ */
+var numIslands = function (grid) {
+    let res = 0
+
+    const dfs = (m, n) => {
+
+        if (m < 0 || m >= grid.length || n < 0 || n >= grid[0].length) return 0
+        if (grid[m][n] === '0') return 0
+        let area = 0
+        grid[m][n] = '0'
+        area = 1 + dfs(m - 1, n)
+            + dfs(m + 1, n)
+            + dfs(m, n - 1)
+            + dfs(m, n + 1)
+        return area
+    }
+
+    for (let i = 0; i < grid.length; i++) {
+        for (let j = 0; j < grid[0].length; j++) {
+            if (dfs(i, j)) {
+                res++
+            }
+        }
+    }
+
+    return res
+}
+
+/* 
+    2021/9/12
+    35 27
+    dfs遍历，遍历到的置为0
+    遍历每一个位置，是岛屿的话(面积不为0)res+1
+    数组元素类型是字符串。。
+*/
+
+let grid = [
+    ["1", "1", "1", "1", "0"],
+    ["1", "1", "0", "1", "0"],
+    ["1", "1", "0", "0", "0"],
+    ["0", "0", "0", "0", "0"]
+]
+numIslands(grid)
