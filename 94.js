b35edaa04a607fcde4be9b46542bfe3555600a1a
SambacFeng <feng20000310@qq.com>
2021-10-03 16:42:46 +0800

    Update 94.js

--- 94.js
+++ 94.js
@@ -11,6 +11,21 @@
  * @return {number[]}
  */
 var inorderTraversal = function (root) {
+    let res = []
+    let stack = []
+    while (root || stack.length) {
+        while (root) {
+            stack.push(root)
+            root = root.left
+        }
+        root = stack.pop()
+        res.push(root.val)
+        root = root.right
+    }
+    return res
+}
+
+/* var inorderTraversal = function (root) {
     let ans = []
     inOrder(root, ans)
     return ans
@@ -20,10 +35,12 @@ let inOrder = (root, res) => {
     inOrder(root.left, ans)
     ans.push(root.val)
     inOrder(root.right, ans)
-}
+} */
 
-/* 
+/*
     2021/8/8
     57% 99%
     好像递归是省空间些
+    2021/10/3
+    补充迭代解法
 */
