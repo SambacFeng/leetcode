94bdf19c6235b67b0761a98d9484313812b3ba4a
SambacFeng <feng20000310@qq.com>
2021-11-02 23:11:41 +0800

    Update 51.js

--- 51.js
+++ 51.js
@@ -2,24 +2,28 @@
  * @param {number} n
  * @return {string[][]}
  */
-var solveNQueens = function(n) {
-    let board = new Array(n).fill(new Array(n).fill('.'))
+var solveNQueens = function (n) {
+    let board = new Array(n).fill(0).map(_ => new Array(n).fill('.'))
     let res = []
     const backtrack = (board, row) => {
         if (row === n) {
-            res.push(board)
+            let subRes = []
+            board.forEach(e => {
+                subRes.push(e.join(''))
+            })
+            res.push(Array.from(subRes))
             return
         }
         for (let col = 0; col < n; col++) {
-            if (!isValid(board, row, col))
-                continue
-            board[row][col] = 'Q'
-            backtrack(board, row + 1)
-            board[row][col] = '.'
+            if (isValid(row, col)) {
+                board[row][col] = 'Q'
+                backtrack(board, row + 1)
+                board[row][col] = '.'
+            }
         }
     }
-    const isValid = (board, row, col) => {
-        for (let i = 0; i < n; i++) {
+    const isValid = (row, col) => {
+        for (let i = 0; i < row; i++) {
             if (board[i][col] === 'Q') {
                 return false
             }
@@ -37,9 +41,15 @@ var solveNQueens = function(n) {
         return true
     }
     backtrack(board, 0)
-    console.log(res);
     return res
-};
+}
+
+/* 
+    2021/11/2
+    26 33
+    回溯
+    二维数组不能使用Array.from的方式获得浅拷贝
+*/
 
 solveNQueens(4)
 // console.log(res);
