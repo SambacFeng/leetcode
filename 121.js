c13534e13bbe7b725d3b54dbe61dd66e54c5a424
SambacFeng <feng20000310@qq.com>
2021-11-21 14:36:51 +0800

    Update 121.js

--- 121.js
+++ 121.js
@@ -26,7 +26,7 @@ var maxProfit = function(prices) {
     let dp = new Array(n + 1).fill(0).map(_ => new Array(2).fill(0))
     dp[0][0] = -prices[0]
     for (let i = 1; i < n; i++) {
-        dp[i][0] = Math.max(dp[i - 1][0], prices[i])
+        dp[i][0] = Math.max(dp[i - 1][0], -prices[i])
         dp[i][1] = Math.max(dp[i - 1][1], dp[i - 1][0] + prices[i])
     }
     
