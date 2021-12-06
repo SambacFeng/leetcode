3a95669556c0024c20299e0c32f9eb564dd8d49f
SambacFeng <feng20000310@qq.com>
2021-12-06 14:27:03 +0800

    Create 1816.js

---
+++ 1816.js
@@ -0,0 +1,15 @@
+/**
+ * @param {string} s
+ * @param {number} k
+ * @return {string}
+ */
+var truncateSentence = function (s, k) {
+    return s.split(' ').slice(0, k).join(' ')
+}
+
+/* 
+    2021/12/6
+    19 24
+    字符串
+    调方法
+*/
