e2434fe0e7a1a4c2e20e080985c84fba371a7300
SambacFeng <feng20000310@qq.com>
2022-03-24 00:08:43 +0800

    Update 53.ts

--- 53.ts
+++ 53.ts
@@ -1,13 +1,19 @@
 function maxSubArray(nums: number[]): number {
-    const n: number = nums.length
-    let prev: number = nums[0], curr: number = 0
     let res: number = nums[0]
+    nums.reduce((prev, curr) => {
+        const value = Math.max(prev + curr, curr)
+        res = Math.max(res, value)
+        return value
+    }, 0)
+    return res
+/*     const n: number = nums.length
+    let prev: number = nums[0], curr: number = 0
     for (let i = 1; i < n; i++) {
         curr = Math.max(nums[i], nums[i] + prev)
         res = Math.max(res, curr)
         prev = curr
     }
-    return res
+    return res */
 }
 
 /* 
